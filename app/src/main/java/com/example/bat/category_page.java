12f0b5497fa3b53e15c90834482f53208191630d
Sahilshelke <90325189+Saahil-Shelke@users.noreply.github.com>
2023-01-04 23:39:14 +0530

    manual_entry

---
+++ app/src/main/java/com/example/bat/category_page.java
@@ -0,0 +1,15 @@
+package com.example.bat;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class category_page extends Activity {
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.category_page);
+
+    }
+}
+
+
